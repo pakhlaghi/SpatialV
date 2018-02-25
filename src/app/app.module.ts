cfd01e1f9056e06d676b568cfc63f10f7119a540
Pakhlaghi <pakhlaghi@agl.com.au>
2018-02-25 18:19:24 +1100

    core and shared module are addded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
